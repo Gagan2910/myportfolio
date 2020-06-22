b6bea409f32384a6143322c95cbdf37bd2e172fb
Gagan2910 <gagan.randhawa941@gmail.com>
2020-06-23 07:42:04 +1200

    Updates

---
+++ precache-manifest.d12ac1883197b39c4aad889ff28f68be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99437b545122bb25829e593b9eccc823",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "1488f416ef3b66859316",
+    "url": "/myportfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "f6dfd01aa7da11e0ef64",
+    "url": "/myportfolio/static/css/main.ad5066fb.chunk.css"
+  },
+  {
+    "revision": "1488f416ef3b66859316",
+    "url": "/myportfolio/static/js/2.44a7c48b.chunk.js"
+  },
+  {
+    "revision": "0dd9a239f83b939f989e1a25267f15b1",
+    "url": "/myportfolio/static/js/2.44a7c48b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6dfd01aa7da11e0ef64",
+    "url": "/myportfolio/static/js/main.21ab2d64.chunk.js"
+  },
+  {
+    "revision": "b7233b44fe85679857af",
+    "url": "/myportfolio/static/js/runtime-main.86b85267.js"
+  }
+]);
